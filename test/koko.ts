d28766aac2ff50a6f174530bbdadf1582f0c25d2
Elias Skogevall <elias.skogevall@gmail.com>
2020-11-02 16:54:44 +0100

    Make mocha test for features individually Fixes #14

--- test/koko.ts
+++
@@ -1,9 +0,0 @@
-import { BindingHandler } from "knockout";
-
-const koko: BindingHandler<string> = {
-   init(element: HTMLElement, va) {
-      element.innerText = `koko ${va()}`
-   }
-}
-
-export default koko
