23d23d11cf3667811d5aa3b2af0874438cc0094a
Elias Skogevall <elias.skogevall@gmail.com>
2020-11-22 14:46:14 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,1 @@
-module.exports = JSON.parse(require('fs').readFileSync(__dirname.replace(/\/$|$/, '') + '/.eslintrc.json'))
+module.exports = JSON.parse(require('fs').readFileSync(__dirname.replace(/\/$|$/, '') + '/.eslintrc.json').toString().replace(/\s*\/\/(.+)$/gm, ''))
