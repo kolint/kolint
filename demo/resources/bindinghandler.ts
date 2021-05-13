5fdda28a341aca9f511a034d97eb6b9fe313552e
Martin Skogevall <martin.skogevall@accility.se>
2021-05-13 20:44:39 +0200

    Delete the demo folder with outdated examples.
    Fixes #110

--- demo/resources/bindinghandler.ts
+++
@@ -1,8 +0,0 @@
-import ko from 'knockout'
-import { BindingContext, ChildBindingContext, Overlay } from '../../lib/resources/context'
-type koko<Value = string[]> = {
-	init(element, value: () => Value)
-
-	transformContext<BC extends BindingContext<any>>(input: Value, parentContext: BC): Overlay<ChildBindingContext<Value, BC>, BC>
-}
-export default koko
