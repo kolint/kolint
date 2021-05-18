4be6dead777fa6e54e9b528f54ec39a43a58e157
Elias Skogevall <elias.skogevall@gmail.com>
2021-05-18 10:44:10 +0200

    Update CLI

---
+++ src/cli/old.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+console.log('Tip! Use \'kolint\' instead, if you\'re not using npx.')
+import './cli'
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { createProgram, Program, Reporting } from './program'
 export { Diagnostic, Severity } from './diagnostic'
 export { Binding, BindingData, BindingExpression, BindingName, DiagNode, Document, Node, NodeType, ImportNode } from './parser'
+export { ConfigOptions } from './cli/cli'
