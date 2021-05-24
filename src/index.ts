8e7effa3dfcbc067defc187993bee4c480d7fa16
Elias Skogevall <elias.skogevall@gmail.com>
2021-05-24 21:03:59 +0200

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
+// src
+export { Diagnostic, Severity, diagnostics, DiagnosticDescription } from './diagnostic'
 export { createProgram, Program, Reporting } from './program'
-export { Diagnostic, Severity } from './diagnostic'
-export { Binding, BindingData, BindingExpression, BindingName, DiagNode, Document, Node, NodeType, ImportNode } from './parser/syntax-tree'
+
+// src/cli
 export { ConfigOptions } from './cli'
+
+// src/compiler
+export { Compiler } from './compiler'
+
+// src/parser
+export { parse } from './parser'
+export { createDocument } from './parser/document-builder'
+export { Coordinates, Location, Position } from './parser/location'
+export * from './parser/syntax-tree'
