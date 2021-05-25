b4ae034cc9a06c1dc85a4dc172eac7950c035772
Elias Skogevall <elias.skogevall@gmail.com>
2021-05-25 16:13:44 +0200

    Don't import knockout in context

--- lib/resources/context.d.ts
+++ lib/resources/context.d.ts
@@ -1,5 +1,4 @@
 /// <reference lib="dom" />
-import * as ko from 'knockout'
 /* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export type MaybeReactive<T> = ko.MaybeComputed<T> | ko.MaybeObservable<T>
