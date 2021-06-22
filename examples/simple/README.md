7edbcc02eca2456e129138cb00f66291912980a1
Martin Skogevall <martin.skogevall@accility.se>
2021-06-22 12:48:33 +0200

    Add simple linting example. (#310)
    * Add simple example for linting.
    
    * Update examples/simple/README.md
    
    Co-authored-by: Elias Skogevall <elias.skogevall@gmail.com>
    
    Co-authored-by: Elias Skogevall <elias.skogevall@gmail.com>

---
+++ examples/simple/README.md
@@ -0,0 +1,5 @@
+# Simple
+Simple example without any errors.
+
+- Change current working directory to `./examples/simple`.
+- Run `node ../../bin/kolint.js view.html`
---
+++ examples/simple/viewmodel.ts
@@ -0,0 +1,5 @@
+import * as ko from 'knockout'
+export default class ViewModel {
+	public title = ko.observable('view')
+	public helloWorld = ko.observable('hello world')
+}
